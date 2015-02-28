d3d8733c35500a30fe7b562d748f76a17f2aef48
SooKee <oasookee@gmail.com>
2015-02-28 03:58:05 +0000

    stuff

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running libtoolize..." ; libtoolize --copy --force --install || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
+
+#./configure "$@"
+
